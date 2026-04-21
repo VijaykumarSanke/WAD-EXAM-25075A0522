46e8ba7611028c8d74467810b6ed405804eda1d0
VijaykumarSanke <vijaykumar.sanke7@gmail.com>
2026-04-21 12:11:16 +0530

    Refactor doGet method in ProfileServlet

--- src/main/java/com/week8/ProfileServlet.java
+++ src/main/java/com/week8/ProfileServlet.java
@@ -3,25 +3,37 @@ package com.week8;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-
+import javax.servlet.http.*;
 
 @WebServlet("/ProfileServlet")
 public class ProfileServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 1. Get the existing session (pass 'false' so it doesn't create a new one if none exists)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
         HttpSession session = request.getSession(false);
-        
+
         if (session != null && session.getAttribute("user") != null) {
+
             String username = (String) session.getAttribute("user");
-            response.getWriter().println("Hello again, " + username);
+            String action = (String) session.getAttribute("lastAction");
+
+            Cookie[] cookies = request.getCookies();
+            String cookieUser = "Not found";
+
+            if (cookies != null) {
+                for (Cookie c : cookies) {
+                    if (c.getName().equals("username")) {
+                        cookieUser = c.getValue();
+                    }
+                }
+            }
+
+            response.getWriter().println("Session User: " + username);
+            response.getWriter().println("<br>Cookie User: " + cookieUser);
+            response.getWriter().println("<br>Last Action: " + action);
+
         } else {
             response.getWriter().println("Please login first.");
         }
-
     }
 }
