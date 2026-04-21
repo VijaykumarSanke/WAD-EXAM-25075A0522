d12c57eb4a9e4e85e2e077c0815b14d60bb8ec5f
VijaykumarSanke <vijaykumar.sanke7@gmail.com>
2026-04-21 12:12:22 +0530

    Clean up comments in ReadCookieServlet
    Removed comments for cookie retrieval and loop.

--- src/main/java/com/week8/ReadCookieServlet.java
+++ src/main/java/com/week8/ReadCookieServlet.java
@@ -13,12 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ReadCookieServlet")
 public class ReadCookieServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 1. Retrieve all cookies from the request
         Cookie[] cookies = request.getCookies();
         String username = "Guest";
         
         if (cookies != null) {
-            // 2. Loop through to find your specific cookie
             for (Cookie cookie : cookies) {
                 if ("username".equals(cookie.getName())) {
                     username = cookie.getValue();
