88055b361448b1c6c0ccc8a275b305948d02a8bc
VijaykumarSanke <vijaykumar.sanke7@gmail.com>
2026-04-21 12:10:26 +0530

    Enhance LoginServlet with session and cookie support
    Added cookie handling for user login and improved session management.

--- src/main/java/com/week8/LoginServlet.java
+++ src/main/java/com/week8/LoginServlet.java
@@ -3,30 +3,26 @@ package com.week8;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import javax.servlet.http.*;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
         String username = request.getParameter("username");
-        
-        // 1. Get the session (creates a new one if it doesn't exist)
+
         HttpSession session = request.getSession();
-        
-        // 2. Store user data in the session
         session.setAttribute("user", username);
-        
-        response.getWriter().println("Welcome, " + username + "! Session created.");
+
+        Cookie userCookie = new Cookie("username", username);
+        userCookie.setMaxAge(3600); // 1 hour
+        response.addCookie(userCookie);
+
+        session.setAttribute("lastAction", "Logged in successfully");
+
+        response.getWriter().println("Welcome, " + username);
+        response.getWriter().println("<br>Session + Cookie created");
         response.getWriter().println("<br><a href='ProfileServlet'>Go to Profile</a>");
     }
-    
-    
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("Please submit a form using POST to login.");
-    }
-
 }
